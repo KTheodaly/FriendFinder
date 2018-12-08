9adb8aae83f7faaa69bc8b475a351c2cbda3ce4b
Theo Daly <theo.daly@outlook.com>
2018-12-08 15:53:15 -0600

    updated readme

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,17 +1,13 @@
 htmlRoutes = function () {
 
-    app.get("/home", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
 
-
-    // app.get("*", function (req, res) {
-    //   res.sendFile(path.join(__dirname, "../public/home.html"));
-    // });
 
     ///to get to the survey page
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    app.get("/home", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
 
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
-var mysql = require("mysql");
+var path = require("path");
+//just saw that path was required while creating the readme
+
 require("./app/routing/apiRoutes.js");
 require("./app/routing/htmlRoutes.js");
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
@@ -17,6 +19,8 @@ var connection = mysql.createConnection({
     password: "",
     database: ""
 });
+sql connection not needed?
+*/
 
 connection.connect(function (err) {
     if (err) throw err;
