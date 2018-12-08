b8236dd45a6d4d143dc547d5e00ddbf5de528610
Theo Daly <theo.daly@outlook.com>
2018-12-08 16:26:34 -0600

    updated stock info

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,54 @@
+var surveyArray = [
+    {
+        "name": "Kissin Kate Barlow",
+        "photo": "src =../assets/Kate_Barlow.jpg",
+        "scores": [
+            3,
+            6,
+            4,
+            2,
+            1,
+            5,
+            7,
+            9,
+            3,
+            8
+        ]
+    },
+    {
+        "name": "Finn the Human",
+        "photo": "src=../assets/Finn_Human.jgp",
+        "scores": [
+            4,
+            3,
+            2,
+            8,
+            8,
+            1,
+            9,
+            3,
+            6,
+            7
+        ]
+    },
+    {
+        "name": "Juni Cortez",
+        "photo": "src=../assets/Juni_Cortez.jpg",
+        "scores": [
+            5,
+            6,
+            4,
+            7,
+            3,
+            8,
+            2,
+            9,
+            1,
+            6
+        ]
+    },
+];
 
+module.exports = surveyArray;
 
 
